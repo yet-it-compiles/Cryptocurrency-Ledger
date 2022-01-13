0d98a8ee6d986b9e199467da29db9487e9ed94c2
Tyler S. Unsworth <43221618+yet-it-compiles@users.noreply.github.com>
2022-01-13 10:01:03 -0800

    Add files via upload
    Removed OO structure from file, added return_enrollment_info() which captures the user input, and for now simply displays it on the console. Also added a TODO list for this module

--- enrollment_user_interface.py
+++ enrollment_user_interface.py
@@ -1,49 +1,76 @@
 """ Simple UI module which contains the logic to display an enrollment screen for a new user to register """
+import tkinter
 from tkinter import *
 
+"""
+TODO 
+- Capture the input information entered by the user - Completed
+- Record this information -Design Decision- [Dictionary would make sense, else send to a .txt file]
+- Send an email to the user saying that have successfully registered their account
+- 
+"""
 
-class EnrollmentScreen:
-    enrollment_screen = Tk()
 
-    def __init__(self):
-        """ Initializes the enrollment screen objects """
-        sign_in_canvas = Canvas(self.enrollment_screen, bg="#ffffff", height=600, width=1000, bd=0, highlightthickness=0
-                                , relief="ridge")
-        sign_in_canvas.place(x=0, y=0)
+def return_enrollment_info():
+    """ Prints the email, name, and password to the console """
+    user_email_entry = user_email.get()
+    user_password_entry = user_password.get()
+    user_phone_number_entry = user_phone_number.get()
 
-        # Initializes the main screen size and sets the color to white
-        self.enrollment_screen.geometry("1000x600")
-        self.enrollment_screen.configure(bg="#ffffff")
+    print("Should be the name ", user_email_entry)
+    print("Should be the name ", user_password_entry)
+    print("Should be the name ", user_phone_number_entry)
 
-        # Initializes background objects
-        background_img = PhotoImage(file=f"background.png")
-        background = sign_in_canvas.create_image(500.5, 300.0, image=background_img)
+    # Clears the entry when the user clicks "Get Started
+    user_email.set("")
+    user_password.set("")
+    user_phone_number.set("")
 
-        # Initializes user entry box objects 1-2
-        entry0_img = PhotoImage(file=f"img_textBox0.png")
-        entry0_bg = sign_in_canvas.create_image(722.5, 179.5, image=entry0_img)
-        entry0 = Entry(bd=0, bg="#e9e9e9", highlightthickness=0)
-        entry0.place(x=588.5, y=154, width=268.0, height=49)
 
-        entry1_img = PhotoImage(file=f"img_textBox1.png")
-        entry1_bg = sign_in_canvas.create_image(722.5, 298.5, image=entry1_img)
-        entry1 = Entry(bd=0, bg="#e9e9e9", highlightthickness=0)
-        entry1.place(x=588.5, y=273, width=268.0, height=49)
+enrollment_screen = Tk()
 
-        entry2_img = PhotoImage(file=f"img_textBox2.png")
-        entry2_bg = sign_in_canvas.create_image(722.5, 417.5, image=entry2_img)
-        entry2 = Entry(bd=0, bg="#e9e9e9", highlightthickness=0)
-        entry2.place(x=588.5, y=392, width=268.0, height=49)
+sign_in_canvas = Canvas(enrollment_screen, bg="#ffffff", height=600, width=1000, bd=0, highlightthickness=0
+                        , relief="ridge")
+sign_in_canvas.place(x=0, y=0)
 
-        splash_image = PhotoImage(file=f"img0.png")  # Crypto picture that shows on the enrollment screen
+# Initializes the main screen size and sets the color to white
+enrollment_screen.geometry("1000x600")
+enrollment_screen.configure(bg="#ffffff")
 
-        # Initializes user entry box objects 1-2
-        b0 = Button(image=splash_image, borderwidth=0, highlightthickness=0, relief="flat")
-        b0.place(x=636, y=481, height=53)
+# Initializes background objects
+background_img = PhotoImage(file=f"background.png")
+background = sign_in_canvas.create_image(500.5, 300.0, image=background_img)
 
-        self.enrollment_screen.resizable(False, False)
-        self.enrollment_screen.mainloop()
+user_email = tkinter.StringVar()
+user_password = tkinter.StringVar()
+user_phone_number = tkinter.StringVar()
 
+# Initializes user entry box objects 1-2
+email_entry_img = PhotoImage(file=f"img_textBox0.png")
+email_background = sign_in_canvas.create_image(722.5, 179.5, image=email_entry_img)
+email_entry = Entry(textvariable=user_email, bd=0, bg="#e9e9e9", highlightthickness=0)
+email_entry.place(x=588.5, y=154, width=268.0, height=49)
+
+entry1_img = PhotoImage(file=f"img_textBox1.png")
+entry1_bg = sign_in_canvas.create_image(722.5, 298.5, image=entry1_img)
+entry1 = Entry(textvariable=user_password, bd=0, bg="#e9e9e9", highlightthickness=0)
+entry1.place(x=588.5, y=273, width=268.0, height=49)
+
+entry2_img = PhotoImage(file=f"img_textBox2.png")
+entry2_bg = sign_in_canvas.create_image(722.5, 417.5, image=entry2_img)
+entry2 = Entry(textvariable=user_phone_number, bd=0, bg="#e9e9e9", highlightthickness=0)
+entry2.place(x=588.5, y=392, width=268.0, height=49)
+
+splash_image = PhotoImage(file=f"img0.png")  # Crypto picture that shows on the enrollment screen
+
+# Initializes user entry box objects 1-2
+b0 = Button(image=splash_image, borderwidth=0, highlightthickness=0, relief="flat", command=return_enrollment_info)
+b0.place(x=636, y=481, height=53)
+
+enrollment_screen.resizable(False, False)
+enrollment_screen.mainloop()
+name = entry1.get()
+print("Name")
 
 if __name__ == '__main__':
-    display_enrollment_screen = EnrollmentScreen()
+    pass
