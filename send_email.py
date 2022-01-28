5eb12141beb44df3de6609d657be3d9c09bee569
Tyler S. Unsworth <43221618+yet-it-compiles@users.noreply.github.com>
2022-01-28 14:23:31 -0800

    Encapsulated password information
    Completed the .env implementation for encapsulating the password

--- send_email.py
+++ send_email.py
@@ -1,4 +1,5 @@
 """ Simple module which will email the member who just enrolled by taking the email address from enrollment_info """
+
 import enrollment_user_interface as enrollment_info
 import smtplib
 import ssl
@@ -12,17 +13,20 @@ port = 465  # Establishes localhost for SSL
 smtp_server = "smtp.gmail.com"
 crypto_ledger_email = "cryptocurrencyledger380@gmail.com"  # Enter your address
 new_member_email = enrollment_info.return_user_information()  # Enter receiver address
-_crypto_ledger_pass = os.getenv('PASSWORD')  # Encapsulated/private variable
-password = _crypto_ledger_pass
+_PASSWORD = os.getenv('EMAIL_PASSWORD')  # Encapsulated/private variable
+_CRYPTO_LEDGER_PASS = _PASSWORD
+
 
 message_to_send = """\
 Subject: Thanks for registering!
 
-Your account has successfully been created!"""
+Your account has successfully been created! 
+
+"""
 
 establish_secure_connection = ssl.create_default_context()
 
 # Declaration of logic to send email to user
 with smtplib.SMTP_SSL(smtp_server, port, context=establish_secure_connection) as server:
-    server.login(crypto_ledger_email, password)
+    server.login(crypto_ledger_email, _CRYPTO_LEDGER_PASS)
     server.sendmail(crypto_ledger_email, new_member_email, message_to_send)
