d34d9b130d110d673b5bf9f6fe4b0c165a4b0199
Tyler S. Unsworth <43221618+yet-it-compiles@users.noreply.github.com>
2022-02-04 09:55:39 -0800

    Button activebackground fixed
    Fixed the improper active background that displayed when the button was pressed

--- user_interface.py
+++ user_interface.py
@@ -1,10 +1,9 @@
-""" TODO """
+""" This module configures each page of the Cryptocurrency ledger """
 import tkinter as tk
 from tkinter import *
 
 
 class TkinterApp(tk.Tk):
-
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
 
@@ -45,7 +44,7 @@ class TkinterApp(tk.Tk):
 
 
 class LoginPage(tk.Frame):
-    """ Displays the login page """
+    """ Configures, and displays the login page """
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -58,38 +57,42 @@ class LoginPage(tk.Frame):
 
         # Grabs the background image, and applies it
         self.login_background = PhotoImage(file=f"login_background.png")
-        background = login_canvas.create_image(395.0, 300.0, image=self.login_background)
+        login_canvas.create_image(395.0, 300.0, image=self.login_background)
 
         # Logic to populate the window
         self.sign_in_button = PhotoImage(file=f"sign_in_button.png")
         sign_in_button_location = Button(self, image=self.sign_in_button, borderwidth=0, highlightthickness=0,
-                                         relief="flat")
+                                         relief="flat", activebackground="#343333")
         sign_in_button_location.place(x=659, y=417, width=159, height=53)
 
+        # Creates, and displays the forgot password button
         self.forgot_password_button = PhotoImage(file=f"forgot_password_button.png")
         forgot_password_location = Button(self, image=self.forgot_password_button, borderwidth=0, highlightthickness=0,
-                                          relief="flat")
+                                          relief="flat", activebackground="#343333")
         forgot_password_location.place(x=444, y=537, width=142, height=50)
 
+        # Creates, and displays the sign-up button
         self.sign_up_button = PhotoImage(file=f"sign_up_button.png")
         sign_up_button_location = Button(self, image=self.sign_up_button, borderwidth=0, highlightthickness=0,
-                                         relief="flat"
+                                         relief="flat", activebackground="#343333"
                                          , command=lambda: controller.show_frame(Enrollment))
 
         sign_up_button_location.place(x=864, y=537, width=136, height=46)
 
+        # Creates, and initializes the text boxes
         self.login_textbox_one = PhotoImage(file=f"login_textbox.png")
-        textbox_one = login_canvas.create_image(738.5, 263.0, image=self.login_textbox_one)
+        login_canvas.create_image(738.5, 263.0, image=self.login_textbox_one)
         textbox_one_location = Entry(self, bd=0, bg="#696969", highlightthickness=0)
         textbox_one_location.place(x=602.0, y=240, width=273.0, height=44)
 
         self.login_textbox_two = PhotoImage(file=f"login_textbox.png")
-        textbox_two = login_canvas.create_image(738.5, 368.0, image=self.login_textbox_two)
+        login_canvas.create_image(738.5, 368.0, image=self.login_textbox_two)
         textbox_two_location = Entry(self, bd=0, bg="#696969", highlightthickness=0)
         textbox_two_location.place(x=602.0, y=345, width=273.0, height=44)
 
 
 class Enrollment(tk.Frame):
+    """ Configures, and displays the login page """
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -106,27 +109,39 @@ class Enrollment(tk.Frame):
         # Declaration of string variable which captures user entries
 
         self.enrollment_text_box = PhotoImage(file=f"enrollment_textBox.png")
-        enrollment_text_background = enrollment_canvas.create_image(722.5, 177.0, image=self.enrollment_text_box)
+        enrollment_canvas.create_image(722.5, 176.0, image=self.enrollment_text_box)
         enrollment_text_box_location = Entry(self, bd=0, bg="#696969", highlightthickness=0)
-        enrollment_text_box_location.place(x=586.0, y=154, width=273.0, height=44)
+        enrollment_text_box_location.place(x=586.0, y=153, width=273.0, height=44)
 
         self.enrollment_text_box_2 = PhotoImage(file=f"enrollment_textBox.png")
-        enrollment_text_background_2 = enrollment_canvas.create_image(722.5, 293.0, image=self.enrollment_text_box_2)
+        enrollment_canvas.create_image(722.5, 293.0, image=self.enrollment_text_box_2)
         enrollment_text_box_location_2 = Entry(self, bd=0, bg="#696969", highlightthickness=0)
         enrollment_text_box_location_2.place(x=586.0, y=270, width=273.0, height=44)
 
         self.enrollment_text_box_3 = PhotoImage(file=f"enrollment_textBox.png")
-        enrollment_text_background_3 = enrollment_canvas.create_image(722.5, 408.0, image=self.enrollment_text_box_3)
+        enrollment_canvas.create_image(722.5, 410.0, image=self.enrollment_text_box_3)
         enrollment_text_box_location_3 = Entry(self, bd=0, bg="#696969", highlightthickness=0)
-        enrollment_text_box_location_3.place(x=586.0, y=385, width=273.0, height=44)
+        enrollment_text_box_location_3.place(x=586.0, y=387, width=273.0, height=44)
 
         self.get_started_button = PhotoImage(file=f"enrollment_get_started.png")
         get_started_background = Button(self, image=self.get_started_button, borderwidth=0, highlightthickness=0
-                                        , relief="flat")
+                                        , relief="flat", activebackground="#343333")
         get_started_background.place(x=636, y=481, width=161, height=53)
 
         enrollment_canvas.create_text(727.5, 71.5, text="Create Account", fill="#ffffff",
                                       font=("Rosarivo-Regular", int(36.0)))
+
+        self.existing_account = PhotoImage(file=f"enrollment_existing_account.png")
+        existing_account_background = Button(self, image=self.existing_account, borderwidth=0, highlightthickness=0
+                                             , command=lambda: controller.show_frame(LoginPage), relief="flat",
+                                             activebackground="#343333")
+
+        existing_account_background.place(x=618, y=530, width=212, height=51)
+
+
+class Dashboard(tk.Frame):
+    """ """
+    pass
 
 
 # Driver Code
