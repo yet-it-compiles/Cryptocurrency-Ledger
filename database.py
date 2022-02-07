fd62b0c7b90bc32af6bf381e0731f8c56e2238de
groverari <41345908+groverari@users.noreply.github.com>
2022-02-07 03:32:34 -0800

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 import psycopg2
 from psycopg2 import Error
 
+
 class database:
 
     def __init__(self, username):
@@ -11,11 +12,11 @@ class database:
         '''
         self.username = username
 
-        self.connection = psycopg2.connect(user="postgres",
-                                      password="pynative@#29",
-                                      host="127.0.0.1",
-                                      port="5432",
-                                      database="postgres_db")
+        self.connection = psycopg2.connect(host="ec2-3-232-22-121.compute-1.amazonaws.com",
+                                           database="dilabshsjveo3",
+                                           user="ogjzilgdyfltod",
+                                           password="2a5fff6b7763149071662013def40f9cb2f9f6c8eef3e719f286d8e499ea8471"
+                                           )
         self.cursor = connection.cursor()
 
     @staticmethod
@@ -24,21 +25,48 @@ class database:
         Method for adding the user for the first time. Its static so that
         the whole class does not need to be instantiated
         '''
-        connection = psycopg2.connect(user="postgres",
-                                      password="pynative@#29",
-                                      host="127.0.0.1",
-                                      port="5432",
-                                      database="postgres_db")
 
-        cursor = connection.cursor()
+        try:
+            connection = psycopg2.connect(host="ec2-3-232-22-121.compute-1.amazonaws.com",
+                                               database="dilabshsjveo3",
+                                               user="ogjzilgdyfltod",
+                                               password="2a5fff6b7763149071662013def40f9cb2f9f6c8eef3e719f286d8e499ea8471"
+                                               )
 
-        cursor.execute(f"INSERT INTO users (\'{name}\', \'{email}\', \'{password}\'")
+            cursor = connection.cursor()
+            postgres_insert_query = "INSERT INTO users Values( %s, %s, %s)"
+            record_to_insert = (name, email, password)
 
-        cursor.close()
-        connection.close()
+            cursor.execute(postgres_insert_query, record_to_insert)
+
+            connection.commit()
+
+            count = cursor.rowcount
+            print(count, "Record inserted successfully into mobile table")
+
+        except(Exception, psycopg2.Error) as error:
+            print("Failed to insert record into mobile table", error)
+
+        finally:
+            # closing database connection.
+            if connection:
+                cursor.close()
+                connection.close()
+                print("PostgreSQL connection is closed")
 
     def getPass(self):
 
-        cusor.execute("SELECT * FROM users WHERE")
+        cusor.execute(f"SELECT pass FROM users WHERE  username = {username}")
+        result = cursor.fetchone()
+
+        if result == "None":
+            return error
+        else:
+            return result
+
+    @staticmethod
+    def checkUsername(name):
+        pass
 
 
+database.adduser("hinduhops", "arieshgroevr@gmail", "password")
