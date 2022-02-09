e9756fd56b3a48439c8aaf0957a34d509c4c8785
Tyler S. Unsworth <43221618+yet-it-compiles@users.noreply.github.com>
2022-02-08 19:44:39 -0800

    Added coming soon

--- user_interface.py
+++ user_interface.py
@@ -7,6 +7,7 @@ class TkinterApp(tk.Tk):
     """
     Configures the screen, and
     """
+
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
 
@@ -26,7 +27,7 @@ class TkinterApp(tk.Tk):
         self.collection_of_canvases = {}
 
         # Declaration of logic to iterate through each page layout
-        for each_layout in (LoginPage, Enrollment, Dashboard):
+        for each_layout in (LoginPage, Enrollment, Dashboard, ComingSoon):
             each_canvas = each_layout(canvas_setup, self)
 
             self.collection_of_canvases[each_layout] = each_canvas
@@ -145,8 +146,8 @@ class Enrollment(tk.Frame):
 
 class Dashboard(tk.Frame):
     """ Configures, and displays the Dashboard """
-    def __init__(self, parent, controller):
 
+    def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.config(width=1440, height=1024)
         flash_delay = 100  # Milliseconds.
@@ -221,86 +222,85 @@ class Dashboard(tk.Frame):
         self.dashboard_image = tk.PhotoImage(file=dashboard_image_path)
         dashboard_image_obj = canvas.create_image(0, 120, anchor='nw', image=self.dashboard_image)
         canvas.tag_bind(dashboard_image_obj, "<ButtonRelease-1>",
-                        lambda event: (flash_hidden(dashboard_image_obj), self.btn_clicked("1")))
+                        lambda event: (flash_hidden(dashboard_image_obj), controller.show_frame(ComingSoon)))
 
         # Retrieves the images, and configures the simulated trading button
         simulated_trading_image_path = "dashboard_simulated_trading.png"
         self.simulated_trading_image = tk.PhotoImage(file=simulated_trading_image_path)
         simulated_trading_image_obj = canvas.create_image(0, 230, anchor='nw', image=self.simulated_trading_image)
         canvas.tag_bind(simulated_trading_image_obj, "<ButtonRelease-1>",
-                        lambda event: (flash_hidden(simulated_trading_image_obj), self.btn_clicked("2")))
+                        lambda event: (flash_hidden(simulated_trading_image_obj), controller.show_frame(ComingSoon)))
 
         # Retrieves the images, and configures the charts button
         charts_image_path = "dashboard_charts.png"
         self.charts_image = tk.PhotoImage(file=charts_image_path)
         charts_image_obj = canvas.create_image(0, 340, anchor='nw', image=self.charts_image)
         canvas.tag_bind(charts_image_obj, "<ButtonRelease-1>",
-                        lambda event: (flash_hidden(charts_image_obj), self.btn_clicked("3")))
+                        lambda event: (flash_hidden(charts_image_obj), controller.show_frame(ComingSoon)))
 
         # Retrieves the images, and configures the portfolio button
         portfolio_image_path = "dashboard_portfolio.png"
         self.portfolio_image = tk.PhotoImage(file=portfolio_image_path)
         portfolio_image_obj = canvas.create_image(0, 450, anchor='nw', image=self.portfolio_image)
         canvas.tag_bind(portfolio_image_obj, "<ButtonRelease-1>",
-                        lambda event: (flash_hidden(portfolio_image_obj), self.btn_clicked("4")))
+                        lambda event: (flash_hidden(portfolio_image_obj), controller.show_frame(ComingSoon)))
 
         alarm_image_path = "dashboard_alarms.png"
         self.alarm_image = tk.PhotoImage(file=alarm_image_path)
         alarm_image_obj = canvas.create_image(0, 560, anchor='nw', image=self.alarm_image)
         canvas.tag_bind(alarm_image_obj, "<ButtonRelease-1>",
-                        lambda event: (flash_hidden(alarm_image_obj), self.btn_clicked("5")))
+                        lambda event: (flash_hidden(alarm_image_obj), controller.show_frame(ComingSoon)))
 
         # Retrieves the images, and configures the news button
         news_image_path = "dashboard_news.png"
         self.news_image = tk.PhotoImage(file=news_image_path)
         news_image_obj = canvas.create_image(0, 670, anchor='nw', image=self.news_image)
         canvas.tag_bind(news_image_obj, "<ButtonRelease-1>",
-                        lambda event: (flash_hidden(news_image_obj), self.btn_clicked("6")))
+                        lambda event: (flash_hidden(news_image_obj), controller.show_frame(ComingSoon)))
 
         # Retrieves the images, and configures the settings button
         settings_image_path = "dashboard_settings.png"
         self.settings_image = tk.PhotoImage(file=settings_image_path)
         settings_image_obj = canvas.create_image(0, 780, anchor='nw', image=self.settings_image)
         canvas.tag_bind(settings_image_obj, "<ButtonRelease-1>",
-                        lambda event: (flash_hidden(settings_image_obj), self.btn_clicked("7")))
+                        lambda event: (flash_hidden(settings_image_obj), controller.show_frame(ComingSoon)))
 
         # Retrieves the images, and configures the logout button
         logout_image_path = "dashboard_logout.png"
         self.logout_image = tk.PhotoImage(file=logout_image_path)
         logout_image_obj = canvas.create_image(45, 950, anchor='nw', image=self.logout_image)
         canvas.tag_bind(logout_image_obj, "<ButtonRelease-1>",
-                        lambda event: (flash_hidden(logout_image_obj), self.btn_clicked("8")))
+                        lambda event: (flash_hidden(logout_image_obj), controller.show_frame(ComingSoon)))
 
         # Retrieves the images, and configures the notifications image
         notifications_image_path = "dashboard_notifications.png"
         self.notifications_image = tk.PhotoImage(file=notifications_image_path)
         notifications_image_obj = canvas.create_image(1027, 19, anchor='nw', image=self.notifications_image)
         canvas.tag_bind(notifications_image_obj, "<ButtonRelease-1>",
-                        lambda event: (flash_hidden(notifications_image_obj), self.btn_clicked("9")))
+                        lambda event: (flash_hidden(notifications_image_obj), controller.show_frame(ComingSoon)))
 
         # Retrieves the images, and configures the support image
         support_image_path = "dashboard_support.png"
         self.support_image = tk.PhotoImage(file=support_image_path)
         support_image_obj = canvas.create_image(1155, 16, anchor='nw', image=self.support_image)
         canvas.tag_bind(support_image_obj, "<ButtonRelease-1>",
-                        lambda event: (flash_hidden(support_image_obj), self.btn_clicked("10")))
+                        lambda event: (flash_hidden(support_image_obj), controller.show_frame(ComingSoon)))
 
         # Retrieves the images, and configures the profile image
         notes_image_path = "dashboard_notes.png"
         self.notes_image = tk.PhotoImage(file=notes_image_path)
         notes_image_obj = canvas.create_image(1268, 19, anchor='nw', image=self.notes_image)
         canvas.tag_bind(notes_image_obj, "<ButtonRelease-1>",
-                        lambda event: (flash_hidden(notes_image_obj), self.btn_clicked("11")))
+                        lambda event: (flash_hidden(notes_image_obj), controller.show_frame(ComingSoon)))
 
         # Retrieves the images, and configures the profile image
         profile_image_path = "dashboard_profile_img.png"
         self.profile_image = tk.PhotoImage(file=profile_image_path)
         profile_image_obj = canvas.create_image(1360, 4, anchor='nw', image=self.profile_image)
         canvas.tag_bind(profile_image_obj, "<ButtonRelease-1>",
-                        lambda event: (flash_hidden(profile_image_obj), self.btn_clicked("12")))
+                        lambda event: (flash_hidden(profile_image_obj), controller.show_frame(ComingSoon)))
 
         canvas.create_text(1398.5, 68.5, text="John Doe", fill="#ffffff", font=("Rosarivo-Regular", int(12.0)))
-
 
         def flash_hidden(image_obj):
             """
@@ -308,12 +308,10 @@ class Dashboard(tk.Frame):
 
             :param image_obj: is the image object to hide
             :type : int
-            :return:
+            :return: a hidden button when pressed
             """
-            print(type(image_obj))
             set_state(tk.HIDDEN, image_obj)
             canvas.after(flash_delay, set_state, tk.NORMAL, image_obj)
-
 
         def set_state(state, image_obj):
             """
@@ -327,6 +325,129 @@ class Dashboard(tk.Frame):
 
         bgr_width, bgr_height = self.background_img.width(), self.background_img.height()
 
+
+class ComingSoon(tk.Frame):
+    """ Configures, and displays the Dashboard """
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.config(width=1440, height=1024)
+        flash_delay = 100  # Milliseconds.
+        self.controller = controller
+
+        canvas = tk.Canvas(self, bg="#343333", height=1024, width=1440, bd=0, highlightthickness=0, relief="ridge")
+        canvas.place(x=0, y=0)
+
+        self.background_img = PhotoImage(file=f"coming_soon.png")
+        canvas.create_image(718.0, 512.0, image=self.background_img)
+
+        # Retrieves the images, and configures the dashboard button
+        dashboard_image_path = "dashboard_dashboard.png"
+        self.dashboard_image = tk.PhotoImage(file=dashboard_image_path)
+        dashboard_image_obj = canvas.create_image(0, 120, anchor='nw', image=self.dashboard_image)
+        canvas.tag_bind(dashboard_image_obj, "<ButtonRelease-1>",
+                        lambda event: (flash_hidden(dashboard_image_obj), controller.show_frame(Dashboard)))
+
+        # Retrieves the images, and configures the simulated trading button
+        simulated_trading_image_path = "dashboard_simulated_trading.png"
+        self.simulated_trading_image = tk.PhotoImage(file=simulated_trading_image_path)
+        simulated_trading_image_obj = canvas.create_image(0, 230, anchor='nw', image=self.simulated_trading_image)
+        canvas.tag_bind(simulated_trading_image_obj, "<ButtonRelease-1>",
+                        lambda event: (flash_hidden(simulated_trading_image_obj), controller.show_frame(ComingSoon)))
+
+        # Retrieves the images, and configures the charts button
+        charts_image_path = "dashboard_charts.png"
+        self.charts_image = tk.PhotoImage(file=charts_image_path)
+        charts_image_obj = canvas.create_image(0, 340, anchor='nw', image=self.charts_image)
+        canvas.tag_bind(charts_image_obj, "<ButtonRelease-1>",
+                        lambda event: (flash_hidden(charts_image_obj), controller.show_frame(ComingSoon)))
+
+        # Retrieves the images, and configures the portfolio button
+        portfolio_image_path = "dashboard_portfolio.png"
+        self.portfolio_image = tk.PhotoImage(file=portfolio_image_path)
+        portfolio_image_obj = canvas.create_image(0, 450, anchor='nw', image=self.portfolio_image)
+        canvas.tag_bind(portfolio_image_obj, "<ButtonRelease-1>",
+                        lambda event: (flash_hidden(portfolio_image_obj), controller.show_frame(ComingSoon)))
+
+        alarm_image_path = "dashboard_alarms.png"
+        self.alarm_image = tk.PhotoImage(file=alarm_image_path)
+        alarm_image_obj = canvas.create_image(0, 560, anchor='nw', image=self.alarm_image)
+        canvas.tag_bind(alarm_image_obj, "<ButtonRelease-1>",
+                        lambda event: (flash_hidden(alarm_image_obj), controller.show_frame(ComingSoon)))
+
+        # Retrieves the images, and configures the news button
+        news_image_path = "dashboard_news.png"
+        self.news_image = tk.PhotoImage(file=news_image_path)
+        news_image_obj = canvas.create_image(0, 670, anchor='nw', image=self.news_image)
+        canvas.tag_bind(news_image_obj, "<ButtonRelease-1>",
+                        lambda event: (flash_hidden(news_image_obj), controller.show_frame(ComingSoon)))
+
+        # Retrieves the images, and configures the settings button
+        settings_image_path = "dashboard_settings.png"
+        self.settings_image = tk.PhotoImage(file=settings_image_path)
+        settings_image_obj = canvas.create_image(0, 780, anchor='nw', image=self.settings_image)
+        canvas.tag_bind(settings_image_obj, "<ButtonRelease-1>",
+                        lambda event: (flash_hidden(settings_image_obj), controller.show_frame(ComingSoon)))
+
+        # Retrieves the images, and configures the logout button
+        logout_image_path = "dashboard_logout.png"
+        self.logout_image = tk.PhotoImage(file=logout_image_path)
+        logout_image_obj = canvas.create_image(45, 950, anchor='nw', image=self.logout_image)
+        canvas.tag_bind(logout_image_obj, "<ButtonRelease-1>",
+                        lambda event: (flash_hidden(logout_image_obj), controller.show_frame(ComingSoon)))
+
+        # Retrieves the images, and configures the notifications image
+        notifications_image_path = "dashboard_notifications.png"
+        self.notifications_image = tk.PhotoImage(file=notifications_image_path)
+        notifications_image_obj = canvas.create_image(1027, 19, anchor='nw', image=self.notifications_image)
+        canvas.tag_bind(notifications_image_obj, "<ButtonRelease-1>",
+                        lambda event: (flash_hidden(notifications_image_obj), controller.show_frame(ComingSoon)))
+
+        # Retrieves the images, and configures the support image
+        support_image_path = "dashboard_support.png"
+        self.support_image = tk.PhotoImage(file=support_image_path)
+        support_image_obj = canvas.create_image(1155, 16, anchor='nw', image=self.support_image)
+        canvas.tag_bind(support_image_obj, "<ButtonRelease-1>",
+                        lambda event: (flash_hidden(support_image_obj), controller.show_frame(ComingSoon)))
+
+        # Retrieves the images, and configures the profile image
+        notes_image_path = "dashboard_notes.png"
+        self.notes_image = tk.PhotoImage(file=notes_image_path)
+        notes_image_obj = canvas.create_image(1268, 19, anchor='nw', image=self.notes_image)
+        canvas.tag_bind(notes_image_obj, "<ButtonRelease-1>",
+                        lambda event: (flash_hidden(notes_image_obj), controller.show_frame(ComingSoon)))
+
+        # Retrieves the images, and configures the profile image
+        profile_image_path = "dashboard_profile_img.png"
+        self.profile_image = tk.PhotoImage(file=profile_image_path)
+        profile_image_obj = canvas.create_image(1360, 4, anchor='nw', image=self.profile_image)
+        canvas.tag_bind(profile_image_obj, "<ButtonRelease-1>",
+                        lambda event: (flash_hidden(profile_image_obj), controller.show_frame(ComingSoon)))
+
+        canvas.create_text(1398.5, 68.5, text="John Doe", fill="#ffffff", font=("Rosarivo-Regular", int(12.0)))
+
+        def flash_hidden(image_obj):
+            """
+            Method sets the state of the object, and hides the buttons when they are interacted with
+
+            :param image_obj: is the image object to hide
+            :type : int
+            :return:
+            """
+            set_state(tk.HIDDEN, image_obj)
+            canvas.after(flash_delay, set_state, tk.NORMAL, image_obj)
+
+        def set_state(state, image_obj):
+            """
+            Sets the state of the image object
+
+            :param state: the state to apply to the buttons
+            :param image_obj: is the image object to apply a state on
+            :return: an image object with a state applied
+            """
+            canvas.itemconfigure(image_obj, state=state)
+
+        bgr_width, bgr_height = self.background_img.width(), self.background_img.height()
 
 
 # Driver Code
