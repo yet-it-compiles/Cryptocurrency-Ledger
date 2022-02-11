02b0733332db3dd1d8397a1ecc867fb872f8c2ed
nemunasw <97710351+nemunasw@users.noreply.github.com>
2022-02-11 03:51:53 -0800

    fixed formatting issues and fixed weighted calculator method

--- manual_transaction.py
+++ manual_transaction.py
@@ -4,7 +4,6 @@ from gecko_api import GeckoApi
 import datetime
 import pytz
 
-
 class ManualTransaction:
     
     crypto_name = ""
@@ -14,7 +13,7 @@ class ManualTransaction:
     timezone = ""
     fee = 0
     trade_value = 0
-    buy_or_sell = True
+    buy_or_sell = True # default value: True = Buy; False = Sell
 
     def __init__(self, crypto_name, num_coins_trading, fee, buy_or_sell):
         """
@@ -23,18 +22,15 @@ class ManualTransaction:
         """
         self.crypto_name = crypto_name
         self.num_coins_trading = num_coins_trading
-        self.fee = 0 # I don't know what this fee is, so for now it will best 0
+        self.fee = 0 # I don't know what this fee is, so for now it will be set to 0
         self.buy_or_sell = buy_or_sell
+
+        """ automatically captures other necessary data """
         self.current_price = GeckoApi(crypto_name).get_attribute("current_price")
         self.trade_value = self.trade_value_format(self.current_price, num_coins_trading)
-
-        global utc_date_time
-        global timezone
-        global local_date_time
-        """ captures other necessary data """
-        local_date_time = datetime.datetime.now()
-        timezone = datetime.datetime.now(datetime.timezone.utc).astimezone().tzinfo
-        utc_date_time = datetime.datetime.utcnow() # date time that will be stored
+        self.utc_date_time = datetime.datetime.utcnow() # date time that will be stored
+        self.timezone = datetime.datetime.now(datetime.timezone.utc).astimezone().tzinfo
+        self.local_date_time = datetime.datetime.now()
 
     def weighted_calculator(self, weight_list, value_list):
         """
@@ -47,37 +43,33 @@ class ManualTransaction:
         div = 0
 
         # calculations for weighted average
-        for i in weight_list:
-            sum += i in weight_list * i in value_list
-            div += weight_list
+        for i in range(len(weight_list)):
+            sum += (weight_list[i] * value_list[i])
+            div += weight_list[i]
         
-        return sum/div
+        return float("{0:.2f}".format(float(sum)/div))
 
 
     def return_transaction(self, crypto_name, num_coins_trading, timezone, trade_value, fee, buy_or_sell):
         """ Stores the transaction into a dictionary with a key where it's values is set to [date + time purchased: coin name, the amount] """
-        key = str(str(utc_date_time) + ": " + crypto_name, num_coins_trading)
+        key = str(str(self.utc_date_time) + ": " + crypto_name, num_coins_trading)
         transaction_dictonary = {}
         if key not in transaction_dictonary:
-            transaction_dictonary[key] = {utc_date_time, timezone, crypto_name, num_coins_trading, trade_value, fee, buy_or_sell}
+            transaction_dictonary[key] = {self.utc_date_time, timezone, crypto_name, num_coins_trading, trade_value, fee, buy_or_sell}
         return transaction_dictonary
 
 
-    def quantity_display(self, num_coins_trading):
+    def quantity_display(self, num_coins_trading, buy_or_sell):
         """ 
         This determines the net change and formats of coins for the transaction
         :param num_coins_trading: int
+        :param buy_or_sell: Boolean; True = Buy; False = Sell
         :rtype str
         """
-
-        # Declares ansi escape characters
-        green = '\033[92m'
-        red = "\033[91m"
-
-        if self.buy_or_sell:
-            return green + "+{0:.1f}".format(num_coins_trading) 
+        if buy_or_sell:
+            return "\033[92m +{0:.1f}".format(num_coins_trading)
         else:
-            return red + "-{0:.1f}".format(num_coins_trading)
+            return "\033[91m -{0:.1f}".format(num_coins_trading)
 
 
     def to_local(self, utc_dt):
@@ -99,26 +91,26 @@ class ManualTransaction:
         calculates price by multiplying current_price and num_coins_trading
         :param current_price: int
         :param num_coins_trading: int
-        :rtype int
+        :rtype float
         """
         return "${0:.2f}".format(current_price * num_coins_trading)
 
 
 # for testing
-if __name__ == "__main__":
-    LOCAL_TIMEZONE = datetime.datetime.now(datetime.timezone.utc).astimezone().tzinfo
-    dt = datetime.datetime.now() # is the current time zone
+# if __name__ == "__main__":
+#     LOCAL_TIMEZONE = datetime.datetime.now(datetime.timezone.utc).astimezone().tzinfo
+#     dt = datetime.datetime.now() # is the current time zone
     
-    # print("key: " + str(date_time) + " " + "crypto_name", "num_coins_trading")
-    utc_dt = datetime.datetime.utcnow()
+#     # print("key: " + str(date_time) + " " + "crypto_name", "num_coins_trading")
+#     utc_dt = datetime.datetime.utcnow()
     
-    mt = ManualTransaction("Bitcoin", 2, 0, True)
+#     mt = ManualTransaction("Bitcoin", 2, 0, True)
 
-    print("current UTC date and time: " + str(utc_dt))
-    print("UTC to local conversion: " + str(mt.to_local(utc_dt)))
+#     print("current UTC date and time: " + str(utc_dt))
+#     print("UTC to local conversion: " + str(mt.to_local(utc_dt)))
     
-    # print(str(dt))
-    print(LOCAL_TIMEZONE)
+#     # print(str(dt))
+#     print(LOCAL_TIMEZONE)
 
     
         
